96432c6c13e19634efab664c332f9200082c2bf1
Thomas Good <tom.good17@gmail.com>
2021-06-04 15:22:33 +1000

    Initial commit

--- App.js
+++ App.js
@@ -4,10 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    Home
   );
 }
 
@@ -17,5 +14,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 40
   },
+  titleText: {
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  subText: {
+    fontSize: 20
+  },
+  descText: {
+    textAlign: 'center'
+  }
 });
+
+const Home = 
+<View style={styles.container}>
+  <Text style={styles.titleText}>Prince's Theatre</Text>
+  <Text>{"\n"}</Text>
+  <Text style={styles.subText}>Classic Movies At Home</Text>
+  <Text>{"\n"}</Text>
+  <Text style={styles.descText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt{"\n"}ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+    exercitation</Text>
+  <StatusBar style="auto" />
+</View>
