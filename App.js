1509322d78058e513b4abc953f808c96baa396e3
tomgood18 <t0m@gmx.us>
2021-06-08 18:18:55 +1000

    Movie Card Implementation

--- App.js
+++ App.js
@@ -1,41 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './Components/Home.js'
+import Movies from './Components/Movies.js'
 
 export default function App() {
   return (
-    Home
+    <Home />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 40
-  },
-  titleText: {
-    fontSize: 30,
-    fontWeight: "bold"
-  },
-  subText: {
-    fontSize: 20
-  },
-  descText: {
-    textAlign: 'center'
-  }
-});
 
-const Home = 
-<View style={styles.container}>
-  <Text style={styles.titleText}>Prince's Theatre</Text>
-  <Text>{"\n"}</Text>
-  <Text style={styles.subText}>Classic Movies At Home</Text>
-  <Text>{"\n"}</Text>
-  <Text style={styles.descText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-    incididunt{"\n"}ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-    exercitation</Text>
-  <StatusBar style="auto" />
-</View>
+
+
+
+
